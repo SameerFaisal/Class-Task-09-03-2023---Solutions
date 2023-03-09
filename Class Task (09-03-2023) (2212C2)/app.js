4ea7906b545efd9f1dfe3d3d808a5bbf0c64f5d0
Sameer Faisal <45840879+SameerFaisal@users.noreply.github.com>
2023-03-09 13:30:18 +0500

    Task (09-03-2023)

---
+++ Class Task (09-03-2023) (2212C2)/app.js
@@ -0,0 +1,26 @@
+// var age=+prompt("Enter your age in years")
+// var days=age*365
+
+// document.write("Your age in days is: "+days)
+
+// let number=+prompt("Enter a number")
+
+// if(number>=40&&number<=60){
+//    document.write("yes")
+// }
+// else{
+// document.write("no")
+// }
+
+// let number=+prompt("Enter a number")
+// let diff=number-13
+// if(diff>13){
+//     document.write(number*2)
+// }
+// else if(diff<13){
+//     document.write(number/2)
+// }
+// else{
+//     document.write("Invalid Input")
+// }
+
